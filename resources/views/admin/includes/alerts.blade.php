c5ad835b5ab98eeb8e08263fb70188ecb88dd4ff
Rodolfo Neto <rodolfoneto>
2022-04-28 14:43:30 -0300

    Search do Perfil com GET agora funcionando 100%

--- resources/views/admin/includes/alerts.blade.php
+++ resources/views/admin/includes/alerts.blade.php
@@ -1,7 +1,19 @@
 @if ($errors->any())
     @foreach ($errors->all() as $error)
-        <div class="alert alert-danger">
+        <div class="alert alert-warning">
             <p>{{$error}}</p>
         </div>
     @endforeach
+@endif
+
+@if (session('message'))
+        <div class="alert alert-success">
+            <p>{{ session('message') }}</p>
+        </div>
+@endif
+
+@if (session('error'))
+        <div class="alert alert-danger">
+            <p>{{ session('error') }}</p>
+        </div>
 @endif
